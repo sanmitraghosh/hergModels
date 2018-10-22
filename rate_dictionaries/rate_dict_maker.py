ec80d85f841357959fbb90efa5a49d07654fafd0
Gary Mirams <gary.mirams@gmail.com>
2018-10-22 22:16:53 +0100

    Model 15

--- rate_dictionaries/rate_dict_maker.py
+++ rate_dictionaries/rate_dict_maker.py
@@ -209,6 +209,22 @@ root = os.path.abspath('rate_dictionaries')
 rate_filename = os.path.join(root, model_name + '-priors.p')
 cPickle.dump(rate14, open(rate_filename, 'wb')) 
 #################################################################################
+model_name = 'model-15'
+rate15 = {
+1: [int(0), int(1), 'positive'],
+2: [int(2), int(3),  'negative'],
+3: [int(4), int(5),  'positive'],
+4: [int(6), int(7),  'negative'],
+5: [int(8), int(9),  'positive'],
+6: [int(10), int(11),  'negative'],
+7: [int(12), int(13),  'positive'],
+8: [int(14), int(15),  'negative']
+}
+
+root = os.path.abspath('rate_dictionaries')
+rate_filename = os.path.join(root, model_name + '-priors.p')
+cPickle.dump(rate15, open(rate_filename, 'wb')) 
+#################################################################################
 model_name = 'model-16'
 rate16 = {
 1: [int(0), int(1), 'positive'],
